5a7dfc822ff21c4b35fa7ca290fcc321cc07a718
JakubStrawa <jakub.strawa@icloud.com>
2021-05-29 20:02:51 +0200

    Dodaj pliki projektów.

---
+++ StudentsWpf/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StudentsWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public MVVM.IWindowService WindowService { get; } = new MVVM.WindowService();
+        private StudentsModel StudentsModel { get; } = new StudentsModel();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            StudentsWpf.ViewModels.StudentsViewModel studentsViewModel = new StudentsWpf.ViewModels.StudentsViewModel(StudentsModel);
+            WindowService.Show(studentsViewModel);
+        }
+    }
+}
---
+++ StudentsWpf/Models/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWpf
+{
+    public class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Surname")); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name")); }
+        }
+
+        private uint index;
+        public uint Index
+        {
+            get { return index; }
+            set { index = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Index")); }
+        }
+
+        private string group;
+        public string Group
+        {
+            get { return group; }
+            set { group = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Group")); }
+        }
+
+        public Student( string surname, string name, uint index, string group)
+        {
+            Surname = surname;
+            Name = name;
+            Index = index;
+            Group = group;
+        }
+    }
+}
